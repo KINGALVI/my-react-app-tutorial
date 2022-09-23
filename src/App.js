7cb00295d563f4e2037bdc274f6ca892984ab647
KINGALVI <alvialmahamud@gmail.com>
2022-09-23 19:39:46 +0600

    edit react consept

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 
@@ -23,10 +24,14 @@ function App() {
 
       {/* how to add a plush button in react */}
       <Counter></Counter>
+
+
+      <ExtarnalUsers></ExtarnalUsers>
     </div>
   );
 }
 
+// how to add a normal div in react .
 function Product(props) {
   return (
     <div className='product'>
@@ -50,6 +55,33 @@ function Counter() {
       <h1>count: {count}</h1>
       <button onClick={increaseCount}>increase</button>
       <button onClick={decreaseCount}>decrease</button>
+    </div>
+  )
+}
+
+// how to declare an api in react
+function ExtarnalUsers() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div>
+      <h3>name: {props.name}</h3>
+      <p>email: {props.email}</p>
     </div>
   )
 }
