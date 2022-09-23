afd7b7bd6399352b7ceea88c60c9ff58c186f145
KINGALVI <alvialmahamud@gmail.com>
2022-09-23 17:35:26 +0600

    edit react consept

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const products=[
-    {name:'laptop',price:'14000'},
-    {name:'phone',price:'24000'},
-    {name:'watch',price:'34000'},
-    {name:'tablet',price:'44000'},
+  const products = [
+    { name: 'laptop', price: '14000' },
+    { name: 'phone', price: '24000' },
+    { name: 'watch', price: '34000' },
+    { name: 'tablet', price: '44000' },
   ]
   return (
     <div className="App">
-      
+
       {/* steap-1 */}
       {
-        products.map(product=> <Product name={product.name
+        products.map(product => <Product name={product.name
         } price={product.price}></Product>)
       }
 
       {/* steap-2 */}
       {/* <Product name='laptop' price='393939'></Product>
       <Product name='phone' price='4999'></Product> */}
+
+      {/* how to add a plush button in react */}
+      <Counter></Counter>
     </div>
   );
 }
@@ -31,6 +35,23 @@ function Product(props) {
     </div>
 
   );
+}
+
+// how to add a plush and minush button in react
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseCount = () =>
+    setCount(count+1);
+
+  const decreaseCount=()=>setCount(count-1)
+  
+  return (
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={increaseCount}>increase</button>
+      <button onClick={decreaseCount}>decrease</button>
+    </div>
+  )
 }
 
 export default App;
