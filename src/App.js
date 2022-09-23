c600434366b51448562a9ff902f9fc86df8c258e
KINGALVI <alvialmahamud@gmail.com>
2022-09-23 12:59:43 +0600

    edit react consept

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product name='laptop'></Product>
+      <Product name='phone' price='4999'></Product>
     </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name : {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+
   );
 }
 
